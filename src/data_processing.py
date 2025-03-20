78105a38edfa8d4b7f8857bcef9d4ceb356990e5
viktor <viktorhristovski629@gmail.com>
2025-03-20 19:09:29 +0100

    feat: Working prototype

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,38 +1,41 @@
 from bs4 import BeautifulSoup
-from .validation import ValidTimes,ValidDaysCheck
+from .validation import ValidTimes, ValidDaysCheck
 from .models import Subject
-from datetime import datetime,time
+from datetime import datetime
+from ics import Calendar, Event
 
 def check_tspan_els(text_el):
     if len(text_el.contents) == 0:
         return text_el.get_text()
 
-    text_content=""
+    text_content = ""
     for child in text_el.children:
         text_content += child.text + ' '
     return text_content
 
+
 def parse_time(time_el):
-    time_format='%H:%M'
-    start,end=time_el.split("-")
-    start,end=start.strip(),end.strip()
-    return datetime.strptime(start,time_format).time(), \
-        datetime.strptime(end,time_format).time()
+    time_format = '%H:%M'
+    start, end = time_el.split("-")
+    start, end = start.strip(), end.strip()
+    return datetime.strptime(start, time_format).time(), \
+        datetime.strptime(end, time_format).time()
+
 
 def extract_dates(text_coll):
-    ch_time=ValidTimes()
-    ch_days=ValidDaysCheck()
+    ch_time = ValidTimes()
+    ch_days = ValidDaysCheck()
 
     sorted_days = []  # (x,text)
     sorted_times = []  # (y,text)
-    i_end_time=None
+    i_end_time = None
     for i in range(1, len(text_coll)):
-        el=text_coll[i]
-        txt_el=el.get_text()
+        el = text_coll[i]
+        txt_el = el.get_text()
         if ch_days.check(txt_el):
             sorted_days.append((
                 float(el.next_sibling['x']),
-                txt_el
+                txt_el.lower()
             ))
         elif ch_time.check(txt_el):
             sorted_times.append((
@@ -40,60 +43,79 @@ def extract_dates(text_coll):
                 parse_time(txt_el)
             ))
         else:
-            i_end_time=i
+            i_end_time = i
             break
 
     return sorted_days, sorted_times, i_end_time
 
+
 """
 Find the maximum of all the min elements in a scheudle array.
 """
-def binary_search(arr,coord):
-    left=0
-    right=len(arr)-1
-    results=None
-    while left<=right:
-        mid=(left+right)//2
-        if arr[mid][0] > coord:
-            right=mid-1
-        else:
-            results=arr[mid]
-            left=mid+1
 
-    if left<len(arr) and arr[left][0] <= coord:
-        results=arr[left]
+
+def binary_search(arr, coord):
+    left = 0
+    right = len(arr) - 1
+    results = None
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid][0] > coord:
+            right = mid - 1
+        else:
+            results = arr[mid]
+            left = mid + 1
+
+    if left < len(arr) and arr[left][0] <= coord:
+        results = arr[left]
     return results[1]
 
-def extract_subjects(sorted_days, sorted_times, i_start ,text_coll):
-    subs=[]
-    PROF_DELIMITAR='/'
-    for i in range(i_start, len(text_coll),3):
-        sub_name=check_tspan_els(text_coll[i])
-        sub_profs=check_tspan_els(text_coll[i+1]).split(PROF_DELIMITAR)
-        place=text_coll[i+2].get_text()
-        rect=text_coll[i+2].next_sibling
 
-        day=binary_search(sorted_days,float(rect['x']))
-        start_time=binary_search(sorted_times,float(rect['y']))[0]
-        height=float(rect['height'])
-        end_time=binary_search(sorted_times,
-                               float(rect['y'])+height-0.2*height)[1]
+def extract_subjects(sorted_days, sorted_times, i_start, text_coll):
+    subs = []
+    PROF_DELIMITAR = '/'
+    for i in range(i_start, len(text_coll), 3):
+        sub_name = check_tspan_els(text_coll[i])
+        sub_profs = check_tspan_els(text_coll[i + 1]).split(PROF_DELIMITAR)
+        place = text_coll[i + 2].get_text()
+        rect = text_coll[i + 2].next_sibling
+
+        day = binary_search(sorted_days, float(rect['x']))
+        start_time = binary_search(sorted_times, float(rect['y']))[0]
+        height = float(rect['height'])
+        end_time = binary_search(sorted_times,
+                                 float(rect['y']) + height - 0.2 * height)[1]
 
         subs.append(
             Subject(
-                 sub_name, sub_profs, place, start_time,end_time , day
+                sub_name, sub_profs, place, start_time, end_time, day
             )
         )
     return subs
 
+
 def process():
-    I_VALIDITY_TEXT=-3
-    I_METADATA_TEXT=I_VALIDITY_TEXT
+    I_VALIDITY_TEXT = -3
+    I_METADATA_TEXT = I_VALIDITY_TEXT
 
     with open('./src/index.html', 'r') as f:
         timetable = BeautifulSoup(f.read(), 'html.parser')
-        timetable=timetable.find_all('text')
+        timetable = timetable.find_all('text')
         # validity_text=timetable[I_VALIDITY_TEXT]
-        timetable=timetable[:I_METADATA_TEXT]
-        sorted_days, sorted_times, i_end_time=extract_dates(timetable)
-        [print(sub) for sub in extract_subjects(sorted_days, sorted_times, i_end_time, timetable)]
+        timetable = timetable[:I_METADATA_TEXT]
+        sorted_days, sorted_times, i_end_time = extract_dates(timetable)
+        subjects = extract_subjects(sorted_days, sorted_times, i_end_time, timetable)
+
+        c = Calendar()
+        for sub in subjects:
+            c.events.add(
+                Event(
+                    name=sub.name,
+                    begin=sub.start,
+                    end=sub.end,
+                    location=sub.place,
+                    description=', '.join(sub.profs),
+                )
+            )
+    with open('./src/schedule.ics', 'w') as s_f:
+        s_f.write(c.serialize())
--- src/models.py
+++ src/models.py
@@ -1,12 +1,43 @@
-def date_formate(start_time,end_time,day):
-    return f'{start_time.isoformat()}-{end_time.isoformat()}-{day}'
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+
+def get_monday():
+    now = datetime.now(tz=ZoneInfo('Europe/Skopje'))
+    return now - timedelta(days=now.weekday())
+
+
+class TimeAdjuster:
+    __instance = None
+    __trans_dict = {'понеделник': 0,
+                    'вторник': 1,
+                    'среда': 2,
+                    'четврток': 3,
+                    'петок': 4
+                    }
+    __monday = get_monday()
+
+    @staticmethod
+    def adjust(day,time):
+        weekday=TimeAdjuster.__trans_dict[day]
+        tmp=TimeAdjuster.__monday + timedelta(days=weekday)
+        return tmp.replace(hour=time.hour,minute=time.minute,second=0)
+
+    @staticmethod
+    def instance():
+        if TimeAdjuster.__instance is None:
+            TimeAdjuster.__instance = TimeAdjuster()
+        return TimeAdjuster.__instance
+
 
 class Subject:
-    def __init__(self,name,profs,place,start,end,day):
-        self.name=name
-        self.profs=profs
-        self.place=place
-        self.date=date_formate(start,end,day)
+    def __init__(self, name, profs, place, start, end, day):
+        self.name = name
+        self.profs = profs
+        self.place = place
+        self.start = TimeAdjuster.adjust(day,start)
+        self.end = TimeAdjuster.adjust(day,end)
+        self.day=day
 
     def __str__(self):
-        return f'{self.name} / {self.date} / {self.place} / '
+        return f'{self.name} / {self.day} / {self.start.time()} - {self.end.time()}'
